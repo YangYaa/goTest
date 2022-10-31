9d3f91e0b03a14c7a6ad8d7b514b07a28e0f0442
unknown <1401314792@qq.com>
2022-10-31 13:45:47 +0800

    add chan code

---
+++ chanTest/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var ch chan int
+
+func add_chan(){
+	time.Sleep(10*time.Second)
+	ch <- 1
+}
+func main() {
+	ch = make(chan int, 1)
+	go add_chan()
+
+	for {
+		select {
+		case x := <-ch:
+			fmt.Println(x)
+			goto over
+		default:
+		}
+	}
+	over:
+		fmt.Println("The pro is over")
+}
