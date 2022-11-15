bc1804900a92d30eac6edcc037e9c92bb78a1c5e
unknown <1401314792@qq.com>
2022-11-15 16:11:21 +0800

    add write json array and pretty json file

--- json/marshal.go
+++ json/marshal.go
@@ -3,16 +3,19 @@ package json
 import (
 	"encoding/json"
 	"fmt"
+	"goTest/3rd/pretty"
 	"io/ioutil"
 )
 
 type TestJson struct {
-	Name string `json:name`
+	Name  string `json:name`
+	Array []int  `json:array`
 }
 
 var Json = new(TestJson)
 
 func LoadJsonFile() {
+	//读取文件中的数据
 	data, err := ioutil.ReadFile("../json/test.json")
 	if err != nil {
 		fmt.Println("Read file error: ", err)
@@ -20,4 +23,8 @@ func LoadJsonFile() {
 	}
 	json.Unmarshal(data, Json)
 	fmt.Println("The read json is ", Json)
+	//追加数据到结构体数组中
+	Json.Array = append(Json.Array, 99)
+	dataJson, _ := json.Marshal(Json)
+	ioutil.WriteFile("../json/test.json", pretty.Pretty(dataJson), 0777)
 }
